2a7bb60082b8027f1f6903a309950a026e90a007
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-10-24 11:07:16 +0800

    Run python in vscode

---
+++ aws-ai/aws-bedrock/1.python/demo.sh
@@ -0,0 +1,15 @@
+Serverless API --> Choose a foundation model --> customize the model --> Recieve response in app 
+
+Pricing:
+Model inference --> Model Customisation 
+
+
+Amazon Bedrock  -----Invoke Model----->  Application
+
+We get charged for the Input and Output tokens processed.
+Also we get charged for the model inference and customization 
+
+On-demand and Batch pricing models.
+Provisioned Throughput Models.
+
+
---
+++ aws-ai/demo.sh
@@ -0,0 +1,6 @@
+bharathkumardasaraju@pythondev$ python ./test.py
+hello world!
+bharathkumardasaraju@pythondev$ python --version
+Python 3.14.0
+bharathkumardasaraju@pythondev$ 
+
